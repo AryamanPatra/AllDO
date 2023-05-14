914b7249c3c4000a2b0ba98606b7d8356419d305
Aryaman Patra <aryamanprivate26@gmail.com>
2023-05-15 02:20:05 +0530

    Added Fragments to HomeActivity.java

--- app/src/main/java/com/example/alldo/ui/ui_elements/HomeActivity.java
+++ app/src/main/java/com/example/alldo/ui/ui_elements/HomeActivity.java
@@ -1,10 +1,18 @@
 package com.example.alldo.ui.ui_elements;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.alldo.R;
 import com.example.alldo.databinding.ActivityHomeBinding;
+import com.google.android.material.navigation.NavigationBarView;
 
 import java.util.Objects;
 
@@ -16,5 +24,28 @@ public class HomeActivity extends AppCompatActivity {
         mainBind = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(mainBind.getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        replaceFragment(new TaskFragment());
+        mainBind.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.simpleTask:
+                        replaceFragment(new TaskFragment());
+                        break;
+                    case R.id.weatherTask:
+                        replaceFragment(new WeatherFragment());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    protected void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentFrameLoader,fragment);
+        fragmentTransaction.commit();
     }
 }
