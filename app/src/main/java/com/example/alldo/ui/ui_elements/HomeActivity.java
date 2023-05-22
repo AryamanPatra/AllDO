03362828910d3233e0544c78d8d893afe030db0a
Aryaman Patra <aryamanprivate26@gmail.com>
2023-05-22 20:06:09 +0530

    Added nav drawer, code remaining

--- app/src/main/java/com/example/alldo/ui/ui_elements/HomeActivity.java
+++ app/src/main/java/com/example/alldo/ui/ui_elements/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.alldo.ui.ui_elements;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
@@ -10,15 +11,17 @@ import androidx.fragment.app.FragmentTransaction;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.alldo.R;
 import com.example.alldo.databinding.ActivityHomeBinding;
 import com.example.alldo.databinding.LayoutTaskBinding;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Objects;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     ActivityHomeBinding mainBind;
     LayoutTaskBinding taskBind;
     @Override
@@ -27,7 +30,18 @@ public class HomeActivity extends AppCompatActivity {
         mainBind = ActivityHomeBinding.inflate(getLayoutInflater());
         taskBind = LayoutTaskBinding.inflate(getLayoutInflater());
         setContentView(mainBind.getRoot());
-        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        mainBind.navMenuButtonHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainBind.drawerLayout.openDrawer(mainBind.navDrawerView);
+            }
+        });
+
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFrameLoader,new TaskFragment()).commit();
+            mainBind.navDrawerView.setCheckedItem(R.id.nav_draw_home);
+        }
 
         replaceFragment(new TaskFragment());
         mainBind.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
@@ -46,6 +60,10 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
 
     protected void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -53,4 +71,5 @@ public class HomeActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.fragmentFrameLoader,fragment);
         fragmentTransaction.commit();
     }
+
 }
